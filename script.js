e824d9fb976b942ccf7ca538aa1d8285e7e7459b
Gillian <gillyng2020@gmail.com>
2024-03-08 05:40:03 +0300

    Adding the validations for the birthdate and gender

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ document.getElementById('birthdateForm').addEventListener('submit', function (ev
     const birthdate = new Date(document.getElementById('birthdate').value);
     const gender = document.getElementById('gender').value;
 
-   
+    if (isValidDate(birthdate)) {
+        const dayOfWeek = calculateDayOfWeek(birthdate);
+        const akanName = getAkanName(dayOfWeek, gender);
+        document.getElementById('result').innerText = `Your Akan name is ${akanName}`;
+    } else {
+        alert('Please enter a valid birthdate.');
+    }
 });
