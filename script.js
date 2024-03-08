7be6abe2ccf1cc717a3df7506bdcdf71d60701cf
Gillian <gillyng2020@gmail.com>
2024-03-08 05:39:12 +0300

    Adding the first JS function

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('birthdateForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const birthdate = new Date(document.getElementById('birthdate').value);
+    const gender = document.getElementById('gender').value;
+
+   
+});
