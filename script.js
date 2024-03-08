dcf92f138816fa56a8306997dc8b97b4cd69f51b
Gillian <gillyng2020@gmail.com>
2024-03-08 05:42:22 +0300

    Adding the calculateDayOfWeek function

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ function isValidDate(date) {
     return date instanceof Date && !isNaN(date);
 }
 
+function calculateDayOfWeek(birthdate) {
+    const CC = Math.floor(birthdate.getFullYear() / 100);
+    const YY = birthdate.getFullYear() % 100;
+    const MM = birthdate.getMonth() + 1;
+    const DD = birthdate.getDate();
+
+    let dayOfWeek = (Math.floor(CC / 4) - 2 * CC - 1 + Math.floor(5 * YY / 4) + Math.floor(26 * (MM + 1) / 10) + DD) % 7;
+    if (dayOfWeek < 0) {
+        dayOfWeek += 7;
+    }
+    return dayOfWeek;
+}
+
